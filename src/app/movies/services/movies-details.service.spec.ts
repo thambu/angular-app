a613bdd0c45ce9254918bae8bc47afac571f37c8
Srikanth Muppavarapu <srikanth@msrikanth.net>
2018-10-29 21:44:05 +1100

    Tests

--- src/app/movies/services/movies-details.service.spec.ts
+++ src/app/movies/services/movies-details.service.spec.ts
@@ -1,55 +1,64 @@
-import { TestBed, getTestBed } from '@angular/core/testing';
-import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-
+import { inject, TestBed, getTestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController , TestRequest} from '@angular/common/http/testing';
 import { MoviesDetailsService } from './movies-details.service';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('MoviesDetailsService Test', () => {
-  const API_ENDPOINT : 'https://spring-movies-cf.cfapps.io/';
-  let httpClient: HttpClient;
-  let injector: TestBed;
-  let service: MoviesDetailsService;
   let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
       TestBed.configureTestingModule({
-        imports: [HttpClientTestingModule],
+        imports: [HttpClientTestingModule, HttpClientModule],
         providers: [MoviesDetailsService]
     });
-    injector = getTestBed();
-    service = injector.get(MoviesDetailsService);
-    httpClient = TestBed.get(HttpClient);
-    httpTestingController = injector.get(HttpTestingController);
+    httpTestingController = TestBed.get(HttpTestingController);
   });
 
-  it('should make an outgoing call to get movies list', () => {
-    const testMovie: MovieDetails = {name:'Wonder Woman',
-                          image:'wonderWoman.jpg',
-                          rating:'7.5/10',
-                          genre:'Action',
-                          summary:'When a pilot crashes and tells of conflict in the outside world'};
+  it('should be created', inject([MoviesDetailsService], (service: MoviesDetailsService) => {
+    expect(service).toBeTruthy();
+  }));
 
-    httpClient.get<MovieDetails>(API_ENDPOINT)
-      .subscribe(movie =>
-        expect(movie).toEqual(testMovie)
-      );
-  });
+  it('should make an outgoing call to retrieve movie details', inject([MoviesDetailsService], (service: MoviesDetailsService) => {
+    let userResponse;
+    
+    const data = [
+      {name:'Wonder Woman',
+      image:'wonderWoman.jpg',
+      rating:'7.5/10',
+      genre:'Action',
+      summary: 'Wonder Women Summary'
+     }
+    ];
 
-  it('should through error',() => {
-  let errorResponse;
-  httpClient.get<MovieDetails>(API_ENDPOINT)
-  .subscribe(
-  resp => {
-  },
-  error => {
-  errorResponse = errors;
-  }
-  );
-  httpTestingController.expectOne(API_ENDPOINT).error(new ErrorEvent('network error'), {status:404});
+    service.getMovies()
+    .subscribe(
+      (resp) => {
+          userResponse = resp;
+      }
+    );
+    const req: TestRequest = httpTestingController.expectOne('https://spring-movies-cf.cfapps.io/');
 
-  expect(errorResponse.status).toEqual(404);
-  httpTestingController.verify();
-  });
+    req.flush(data);
 
+    expect(userResponse).toEqual(data);
+    httpTestingController.verify();
+  }));
+
+  it('should throw an error', inject([MoviesDetailsService], (service: MoviesDetailsService) => {
+    let errorResponse;
+
+    service.getMovies()
+    .subscribe(
+      resp => {},
+      err => {
+        errorResponse = err;
+      }
+    );
+
+    httpTestingController.expectOne('https://spring-movies-cf.cfapps.io/').error(new ErrorEvent('network error'), { status: 404});
+
+    expect(errorResponse.status).toEqual(404);
+    httpTestingController.verify();
+  }));
 
 });
